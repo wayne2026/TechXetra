f1c8b809fb420b4f013d7dc76421bf7df081a14c
Nilkamal Sarma <nilkamalsarma12@gmail.com>
2024-09-13 11:29:17 +0530

    adde about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,12 @@
-
-const About = () => {
+import React from 'react';
+import { StarsBackground } from '../../components/StarBackground';
+const About: React.FC = () => {
   return (
-    <div>About Us</div>
-  )
-}
+    <div className='bg-gradient-to-b from-[#020b22] to-[#271938] '>
+      <h1 className='text-white  text-4xl pl-20'>About Us</h1>
+      <StarsBackground/>
+    </div>
+  );
+};
 
-export default About
+export default About;
