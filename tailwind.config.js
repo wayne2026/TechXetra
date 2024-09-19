85b9e11656ae5bb0a7949454a5a4676d3a163b3b
arunavsaha123 <arunavsaha984@gmail.com>
2024-09-19 20:36:51 +0530

    events page added

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        originTech:['originTech'],
+        techno:['techno'],
+        autoTechno:['autoTechno'],
+        lemonMilk:['lemonMilk']
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [],
+}
+
