b33eb2b6a5af0612a7173fa1e4d26a31453d2011
dhritiman78 <dhritiman.saikia.11b.244@gmail.com>
2024-09-21 01:58:59 +0530

    added footerbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Login from './pages/auth/Login'
 import About from './pages/About'
 import PastEvents from './pages/PastEvents'
 import Events from './pages/Events'
+import FooterBar from './pages/Footer'
 function App() {
   return (
     <>
@@ -14,6 +15,7 @@ function App() {
       <About/>
       <Events/>
       <PastEvents />
+      <FooterBar />
     </>
   )
 }
