3cf85f5d8c5331cb0a1668869a361b9ec9bcd22c
dhritiman78 <dhritiman.saikia.11b.244@gmail.com>
2024-09-26 00:33:57 +0530

    added past events

--- src/pages/PastEvents.tsx
+++ src/pages/PastEvents.tsx
@@ -16,31 +16,113 @@ const PastEvents: React.FC = () => {
   const eventRefs = useRef<(HTMLDivElement | null)[]>([]);
 
   const events: Event[] = [
-    {EventName: "Hackathon", Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo minus aperiam nisi officia, impedit fugiat nemo architecto ullam.", Image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6cI51LvrxoqISHcYbGs7aVaWKswEcs8BU0g&s"},
-    {EventName: "Hackathon", Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo minus aperiam nisi officia, impedit fugiat nemo architecto ullam.", Image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6cI51LvrxoqISHcYbGs7aVaWKswEcs8BU0g&s"},
-    {EventName: "Hackathon", Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo minus aperiam nisi officia, impedit fugiat nemo architecto ullam.", Image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6cI51LvrxoqISHcYbGs7aVaWKswEcs8BU0g&s"},
-    {EventName: "Hackathon", Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo minus aperiam nisi officia, impedit fugiat nemo architecto ullam.", Image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6cI51LvrxoqISHcYbGs7aVaWKswEcs8BU0g&s"},
-    {EventName: "Hackathon", Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo minus aperiam nisi officia, impedit fugiat nemo architecto ullam.", Image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6cI51LvrxoqISHcYbGs7aVaWKswEcs8BU0g&s"},
-    {EventName: "Hackathon", Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo minus aperiam nisi officia, impedit fugiat nemo architecto ullam.", Image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6cI51LvrxoqISHcYbGs7aVaWKswEcs8BU0g&s"},
+    {EventName: "Hackathon", Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo minus aperiam nisi officia, impedit fugiat nemo architecto ullam.", Image: "../../public/pastEventsPic.png"},
+    {EventName: "Hackathon", Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo minus aperiam nisi officia, impedit fugiat nemo architecto ullam.", Image: "../../public/pastEventsPic.png"},
+    {EventName: "Hackathon", Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo minus aperiam nisi officia, impedit fugiat nemo architecto ullam.", Image: "../../public/pastEventsPic.png"},
+    {EventName: "Hackathon", Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo minus aperiam nisi officia, impedit fugiat nemo architecto ullam.", Image: "../../public/pastEventsPic.png"},
+    {EventName: "Hackathon", Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo minus aperiam nisi officia, impedit fugiat nemo architecto ullam.", Image: "../../public/pastEventsPic.png"},
+    {EventName: "Hackathon", Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo minus aperiam nisi officia, impedit fugiat nemo architecto ullam.", Image: "../../public/pastEventsPic.png"},
   ];
 
   useEffect(() => {
     eventRefs.current.forEach((ref, index) => {
       if (ref) {
         const isEven = index % 2 === 0;
-        gsap.from(ref, {
-          x: isEven ? "-100vw" : "100vw", // Move from left or right
-          opacity: 0,
+  
+        // Initial image animation
+        gsap.fromTo(
+          ref.querySelector("img"),
+          {
+            x: isEven ? "-150vw" : "150vw",
+            opacity: 0,
+            rotateZ: isEven ? 50 : -50,
+            y: -50,
+            scale: 0.6,
+          },
+          {
+            x: 0,
+            opacity: 1,
+            rotateZ: 0,
+            y: 0,
+            scale: 1,
+            duration: 2.5,
+            ease: "elastic.out(1, 0.7)",
+            scrollTrigger: {
+              trigger: ref,
+              start: "top 85%",
+              end: "top 50%",
+              scrub: true,
+              toggleActions: "play none none none",
+            },
+            onComplete: () => {
+              // Continuous floating
+              gsap.to(ref.querySelector("img"), {
+                y: '+=15',
+                rotationZ: isEven ? 5 : -5,
+                repeat: -1,
+                yoyo: true,
+                duration: 5,
+                ease: "sine.inOut",
+              });
+            },
+          }
+        );
+  
+        // Parallax effect on image
+        gsap.to(ref.querySelector("img"), {
+          y: isEven ? "-=30" : "+=30",  // Move up/down while scrolling
+          ease: "power1.out",
           scrollTrigger: {
             trigger: ref,
-            start: "top 80%",
-            end: "top 50%",
-            scrub: true,
+            start: "top bottom",
+            end: "bottom top",
+            scrub: 1,
           },
         });
+  
+        // Text animations: Stagger fade-in
+        const textBlocks = ref.querySelectorAll(".text-block");
+        gsap.fromTo(
+          textBlocks,
+          { opacity: 0, y: 20 },
+          {
+            opacity: 1,
+            y: 0,
+            duration: 1.5,
+            ease: "power2.out",
+            stagger: 0.2,
+            scrollTrigger: {
+              trigger: ref,
+              start: "top 80%",
+              toggleActions: "play none none none",
+            },
+          }
+        );
+  
+        // Zoom in and blur effect when out of view
+        gsap.fromTo(
+          ref.querySelector("img"),
+          { filter: "blur(5px)", scale: 0.9 },
+          {
+            filter: "blur(0px)",
+            scale: 1,
+            duration: 1.5,
+            ease: "power2.out",
+            scrollTrigger: {
+              trigger: ref,
+              start: "top 70%",
+              end: "top 30%",
+              scrub: true,
+            },
+          }
+        );
       }
     });
   }, []);
+  
+  
+  
+  
 
   return (
     <div className="bg-gradient-to-b from-[#22071b] via-[#190341] to-[#1f021c] relative overflow-x-hidden">
@@ -75,7 +157,11 @@ const PastEvents: React.FC = () => {
                 </div>
               )}
               <div className="flex justify-center">
-                <img className="h-[260px] w-[260px]" src={event.Image} alt="Event" />
+                <img
+                  className="h-[260px] w-[260px] rounded-lg"
+                  src={event.Image}
+                  alt="Event"
+                />
               </div>
               {isEven && (
                 <div className="px-10 my-auto">
