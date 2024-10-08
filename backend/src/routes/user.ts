104b694fb27592d48caae59ef1454cf43b5c5da7
ayanshuDS <73418468+MET4L-DS@users.noreply.github.com>
2024-10-09 00:15:05 +0530

    Implemented authentication using email, password

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,14 +1,30 @@
 import { Router } from "express";
-import { Request, Response } from "express";
 
-import { getAllUsers, getUser, createUser } from "../controllers/user";
+import {
+	getAllUsers,
+	loginUser,
+	registerUser,
+	handleRefreshToken,
+	logoutUser,
+} from "../controllers/user";
+
+import { greet } from "../controllers/greet";
+
+import { authMiddleware } from "../middleware/auth_middleware";
 
 const router = Router();
 
 router.get("/", getAllUsers);
 
-router.get("/:id", getUser);
+// router.get("/:id", loginUser);
+router.post("/login", loginUser);
 
-router.post("/", createUser);
+router.post("/register", registerUser);
+
+router.get("/refresh", handleRefreshToken);
+
+router.get("/logout", logoutUser);
+
+router.get("/greet", authMiddleware, greet);
 
 export default router;
