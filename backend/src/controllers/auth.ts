aee44b792e925f46fb49d0c32554060713453d42
Umananda Siddha <umanandasiddha243@gmail.com>
2024-10-15 21:46:05 +0530

    all set

--- backend/src/controllers/auth.ts
+++
@@ -1,2 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-import { StatusCodes } from "http-status-codes";
--- backend/src/db/connect.ts
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async (URI: string) => {
-	if (!URI) throw new Error("No URI provided");
-	await mongoose.connect(URI);
-};
