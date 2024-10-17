8f483730008f95219569e45deb8de7e170eafed3
Umananda Siddha <umanandasiddha243@gmail.com>
2024-10-18 04:29:41 +0530

    compilable

--- frontend/components/SpaceShip.tsx
+++ frontend/components/SpaceShip.tsx
@@ -9,14 +9,13 @@ const Planet: React.FC = () => {
 
     const { scene } = useGLTF("/Drone.glb");
 
-   
     const [floatingOffset, setFloatingOffset] = useState(0);
 
     useFrame((state, delta) => {
+        console.log(state)
         setFloatingOffset(floatingOffset + delta);
         if (planetRef.current) {
             planetRef.current.position.y = Math.sin(floatingOffset) * 0.05;
-           
         }
     });
 
