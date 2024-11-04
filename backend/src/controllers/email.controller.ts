bc7be1eec0297cdc9f1d649ae4d3b3168d6acafd
Umananda Siddha <umanandasiddha243@gmail.com>
2024-11-04 13:58:57 +0530

    email fetch filters

--- backend/src/controllers/email.controller.ts
+++ backend/src/controllers/email.controller.ts
@@ -1,15 +1,28 @@
 import { NextFunction, Request, Response } from "express";
 import EmailModel from "../models/email.model.js";
+import ApiFeatures from "../utils/apiFeatures.js";
+import { StatusCodes } from "http-status-codes";
 
 export const getAllEmails = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const emails = await EmailModel.find();
+        const resultPerPage = 10;
+        const count = await EmailModel.countDocuments();
 
-        res.status(200).json({
+        const apiFeatures = new ApiFeatures(EmailModel.find().sort({ $natural: -1 }), req.query).search().filter();
+
+        let filteredEmails = await apiFeatures.query;
+        let filteredEmailsCount = filteredEmails.length;
+
+        apiFeatures.pagination(resultPerPage);
+        filteredEmails = await apiFeatures.query.clone();
+
+        res.status(StatusCodes.OK).json({
             success: true,
-            count: emails.length,
-            emails
-        })
+            count,
+            resultPerPage,
+            users: filteredEmails,
+            filteredEmailsCount
+        });
     } catch (error) {
         next(error);
     }
